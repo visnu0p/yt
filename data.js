dfea10af0dd3c938edf7248668fc5db2f64b3305
visnu0p <104520802+visnu0p@users.noreply.github.com>
2022-04-28 19:27:10 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://user500:9xFx_e!mpxNbcK!@cluster0.5u03a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5148556903:AAHKNDWf_FS1-xAHvqDA6_3LCst0qC1zYbM',//bot token
+
+bot_name:'litcoinltcautopayvbot',//bot username without @
+
+bot_admin: 2087872161,// admin telegram id
+
+channelsList : ['@hensdrop','@airdropserve','@PheonixDrops','@arenadrops','@AirdropMajesty','@doremonairdrops','@Boompayoutz'],// channels list
+
+reffer_bonus:0.000005,// refferal bonus amount
+
+min_wd:0.00002,//min withdrawal amount
+
+daily_bonus:0.0000001,// daily bonus amount
+
+currency:'SOL', //bot cuurency
+
+cb_api_key:'ZtYEQeNJejoP49Cf',//coinbase api key
+
+cb_api_secret:'hXZuYiR4JJjYPJdTJkzSQDVQTbk2WsJC',// coinbase secret key
+
+cb_account_id:'977a6fba-4171-5b22-9f2a-66e9a02613e2',// counbase account id for the bot currency
+
+payment_channel:'@boompayoutz'// payment channel username
+
+}
